dba12daf294157d8ef28e18af9224488b2fdf25a
madicke <madicke@http://gitsrv.laposte.sn/>
2020-10-10 12:05:41 +0200

    modified server.js file

--- server.js
+++ server.js
@@ -9,10 +9,8 @@ app.use(express.static(__dirname + '/dist/cryptojavafront'));
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname+'/dist/cryptojavafrontr/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/cryptojavafront/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-
